ba66fea19f01a8e5843e949caadd94c784a15f53
alok8bb <alok8bb@gmail.com>
2023-06-25 21:27:38 +0530

    add paging
    Signed-off-by: alok8bb <alok8bb@gmail.com>

--- main.ts
+++ main.ts
@@ -9,8 +9,21 @@ const app = new Application();
 const router = new Router();
 
 router.get("/api/posts", (ctx: Context) => {
+  let page = 0;
+  let pageSize = data.length;
+
+  ctx.request.url.searchParams.forEach((v, k) => {
+    if (k == "page") {
+      page = Number(v);
+    }
+
+    if (k == "pageSize") {
+      pageSize = Number(v);
+    }
+  });
+
   ctx.response.status = 200;
-  ctx.response.body = data;
+  ctx.response.body = data.slice(pageSize * page, (pageSize * page) + pageSize);
 });
 
 app.use(router.allowedMethods());
