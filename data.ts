1496ccfe08adc179a07990b1a04720c43e9f081e
alok8bb <alok8bb@gmail.com>
2023-06-26 07:58:14 +0530

    update data
    Signed-off-by: alok8bb <alok8bb@gmail.com>

--- data.ts
+++ data.ts
@@ -1,4 +1,4 @@
-export const data = [
+export const old = [
   {
     "author_image": "https://i.pravatar.cc/700",
     "author_name": "Alok Pawar",
@@ -282,6 +282,239 @@ export const data = [
         "user_image": "https://i.pravatar.cc/700",
         "comment": "I'll spread the word!",
         "likes": 5,
+      },
+    ],
+  },
+];
+
+export const data = [
+  {
+    "author_image": "https://i.pravatar.cc/700",
+    "author_name": "Elena Jones",
+    "post_type": "Question",
+    "likes": 1300,
+    "text": "What a beautiful day",
+    "attachment_type": "image",
+    "attachments": [
+      "https://images.unsplash.com/photo-1687579520892-5160c0df4b3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=600&q=60",
+      "https://images.unsplash.com/photo-1687166783902-45947281f59d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=600&q=60",
+      "https://images.unsplash.com/photo-1687565021770-cdb4e32002a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    ],
+    "comments": [
+      {
+        "username": "John Doe",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "random string",
+        "likes": 10,
+      },
+    ],
+  },
+  {
+    "author_image": "https://i.pravatar.cc/700",
+    "author_name": "Maxwell Smith",
+    "post_type": "Marketing",
+    "likes": 783,
+    "text": "What an amazing offer!",
+    "attachment_type": "audio",
+    "attachments": [
+      "https://example.com/audio1.mp3",
+      "https://example.com/audio2.mp3",
+    ],
+    "comments": [
+      {
+        "username": "John Doe",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "Great post!",
+        "likes": 20,
+      },
+      {
+        "username": "Jane Smith",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "I love it!",
+        "likes": 15,
+      },
+    ],
+  },
+  {
+    "author_image": "https://i.pravatar.cc/700",
+    "author_name": "Sophia Brown",
+    "post_type": "Question",
+    "likes": 950,
+    "text": "Any recommendations for a good book?",
+    "attachment_type": "video",
+    "attachments": [
+      "https://example.com/video1.mp4",
+    ],
+    "comments": [
+      {
+        "username": "John Doe",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "Check out 'The Alchemist' by Paulo Coelho!",
+        "likes": 8,
+      },
+      {
+        "username": "Emily Johnson",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "I suggest 'To Kill a Mockingbird' by Harper Lee.",
+        "likes": 12,
+      },
+    ],
+  },
+  {
+    "author_image": "https://i.pravatar.cc/700",
+    "author_name": "Liam Davis",
+    "post_type": "Marketing",
+    "likes": 421,
+    "text": "Limited time offer! Get 50% off.",
+    "attachment_type": "image",
+    "attachments": [
+      "https://example.com/image1.jpg",
+      "https://example.com/image2.jpg",
+    ],
+    "comments": [],
+  },
+  {
+    "author_image": "https://i.pravatar.cc/700",
+    "author_name": "Ava Wilson",
+    "post_type": "Question",
+    "likes": 1100,
+    "text": "What's your favorite holiday destination?",
+    "attachment_type": "audio",
+    "attachments": [
+      "https://example.com/audio1.mp3",
+      "https://example.com/audio2.mp3",
+      "https://example.com/audio3.mp3",
+    ],
+    "comments": [
+      {
+        "username": "John Doe",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "I love visiting Bali!",
+        "likes": 14,
+      },
+      {
+        "username": "Sarah Johnson",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "My favorite is Paris!",
+        "likes": 18,
+      },
+      {
+        "username": "Michael Smith",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "I enjoy exploring Tokyo.",
+        "likes": 6,
+      },
+    ],
+  },
+  {
+    "author_image": "https://i.pravatar.cc/700",
+    "author_name": "Olivia Thomas",
+    "post_type": "Marketing",
+    "likes": 675,
+    "text": "New product alert! Check it out.",
+    "attachment_type": "video",
+    "attachments": [
+      "https://example.com/video1.mp4",
+      "https://example.com/video2.mp4",
+    ],
+    "comments": [
+      {
+        "username": "John Doe",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "Looks interesting!",
+        "likes": 11,
+      },
+      {
+        "username": "Emma Davis",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "Can't wait to try it!",
+        "likes": 9,
+      },
+    ],
+  },
+  {
+    "author_image": "https://i.pravatar.cc/700",
+    "author_name": "Noah Wilson",
+    "post_type": "Question",
+    "likes": 230,
+    "text": "What's your favorite movie of all time?",
+    "attachment_type": "image",
+    "attachments": [
+      "https://example.com/image1.jpg",
+      "https://example.com/image2.jpg",
+      "https://example.com/image3.jpg",
+    ],
+    "comments": [
+      {
+        "username": "John Doe",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "I love 'The Shawshank Redemption'!",
+        "likes": 16,
+      },
+      {
+        "username": "Sophia Brown",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "My favorite is 'Inception'.",
+        "likes": 13,
+      },
+    ],
+  },
+  {
+    "author_image": "https://i.pravatar.cc/700",
+    "author_name": "William Thompson",
+    "post_type": "Marketing",
+    "likes": 512,
+    "text": "Big sale! Don't miss out.",
+    "attachment_type": "audio",
+    "attachments": [
+      "https://example.com/audio1.mp3",
+      "https://example.com/audio2.mp3",
+    ],
+    "comments": [
+      {
+        "username": "John Doe",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "Great deals!",
+        "likes": 7,
+      },
+      {
+        "username": "Emma Wilson",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "I'll definitely check it out.",
+        "likes": 5,
+      },
+    ],
+  },
+  {
+    "author_image": "https://i.pravatar.cc/700",
+    "author_name": "Isabella Davis",
+    "post_type": "Question",
+    "likes": 890,
+    "text": "Looking for new restaurant recommendations.",
+    "attachment_type": "video",
+    "attachments": [
+      "https://example.com/video1.mp4",
+      "https://example.com/video2.mp4",
+      "https://example.com/video3.mp4",
+    ],
+    "comments": [],
+  },
+  {
+    "author_image": "https://i.pravatar.cc/700",
+    "author_name": "James Wilson",
+    "post_type": "Marketing",
+    "likes": 310,
+    "text": "Introducing our latest product.",
+    "attachment_type": "image",
+    "attachments": [
+      "https://example.com/image1.jpg",
+    ],
+    "comments": [
+      {
+        "username": "John Doe",
+        "user_image": "https://i.pravatar.cc/700",
+        "comment": "Looks cool!",
+        "likes": 4,
       },
     ],
   },
